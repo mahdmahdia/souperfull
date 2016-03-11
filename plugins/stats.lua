1c15e868d62025d59c9f68cfc034c307b433973f
mahdmahdia <mahdi028c@gmail.com>
2016-03-11 17:43:54 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (bot)",-- Put everything you like :)
+		"^[!/](bot)"-- Put everything you like :)
     }, 
   run = run
 }
